4e5534aa9fd51cae5604944e8b93c1b20ae72ab6
navabiradar <nava526@outlook.com>
2024-08-19 14:13:02 +0530

    Update

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,7 +10,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover"/>
     <meta http-equiv="X-UA-Compatible" content="ie=edge"/>
     <title></title>
-    <link rel="sitemap" href="/sitemap-index.xml" />
 		
 	<script src="https://cdn.jsdelivr.net/npm/@tabler/core@1.0.0-beta17/dist/js/tabler.min.js"></script>
 
@@ -25,7 +24,7 @@
 
 
 
-		<link rel="icon" type="image/x-icon" href="/favicon.svg">
+	<link rel="icon" type="image/x-icon" href="/favicon.svg">
     <!-- CSS files -->
     
     <link rel="preconnect" href="https://fonts.googleapis.com">
